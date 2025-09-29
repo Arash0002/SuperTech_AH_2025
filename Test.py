1f9a807e2837e49e50d07166f23d3b9cc2548509
Arash <heidari.arash2000@gmail.com>
2025-09-29 14:02:19 +0100

    Created our first demo script

---
+++ Test.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# Arash
+# Version 1.0
+# Description
+# My first script
+# Script
+
+"""
+    DOCSTRING : This is a multi
+    line
+    comment
+"""
+
+## name = "Arash Heydary"
+
+name = input("Enter your name :"ar)
+
+print("My name is", name, sep = " ")
+print("My name is" + name)
+
+import math
+
+print("Cos of 0.5 =", math.cos(0.5))
+
+import random
+print("My lucky number is", random.randint(1, 50))
+
+print("Done")
